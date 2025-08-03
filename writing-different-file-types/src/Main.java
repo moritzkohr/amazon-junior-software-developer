ab8330f6f505e6e0b001173514c814963fdc7bce
Moritz Kohr <morris.ko98@googlemail.com>
2025-08-03 13:38:54 +0200

    updating formating

--- writing-different-file-types/src/Main.java
+++ writing-different-file-types/src/Main.java
@@ -28,7 +28,7 @@ public class Main {
             System.out.println("Enter weight: ");
             double weigth = scanner.nextDouble();
             Person person = new Person(name, age, weigth);
-            data=person.toString();
+            data = person.toString();
             fileObj.println(data);
             fileObj.close();
             System.out.println("File written with PrintWriter...");
@@ -38,35 +38,35 @@ public class Main {
     }
 
     public static void binaryFileWrite() {
-        byte[] numbers ={10,20,30,40,50};
-        try(FileOutputStream fileObj=new FileOutputStream("file2.dat")){
-            for (int i=0;i<numbers.length;i++){
+        byte[] numbers = {10, 20, 30, 40, 50};
+        try (FileOutputStream fileObj = new FileOutputStream("file2.dat")) {
+            for (int i = 0; i < numbers.length; i++) {
                 fileObj.write(numbers[i]);
             }
             System.out.println("File written with FileOutputStream...");
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void streamWrite(){
-        try{
-            FileOutputStream fileObj=new FileOutputStream("file4.dat");
-            DataOutputStream dataObj=new DataOutputStream(fileObj);
-            Scanner scanner=new Scanner(System.in);
+    public static void streamWrite() {
+        try {
+            FileOutputStream fileObj = new FileOutputStream("file4.dat");
+            DataOutputStream dataObj = new DataOutputStream(fileObj);
+            Scanner scanner = new Scanner(System.in);
             System.out.println("Enter name: ");
-            String name=scanner.nextLine();
+            String name = scanner.nextLine();
             System.out.println("Enter age: ");
-            int age=scanner.nextInt();
+            int age = scanner.nextInt();
             System.out.println("Enter weight: ");
-            double weight=scanner.nextDouble();
-            Person person=new Person(name,age,weight);
+            double weight = scanner.nextDouble();
+            Person person = new Person(name, age, weight);
             dataObj.writeUTF(person.name);
             dataObj.writeInt(person.age);
             dataObj.writeDouble(person.weight);
             dataObj.close();
             System.out.println("File written with DataOutputStream...");
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
