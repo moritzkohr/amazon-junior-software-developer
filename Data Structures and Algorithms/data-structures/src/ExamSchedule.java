27a59ee54e26071c9ca20ed4152d3373ea058bcc
Moritz Kohr <morris.ko98@googlemail.com>
2025-08-16 17:17:20 +0200

    feat: copied base code from graded lab to local IDE

---
+++ Data Structures and Algorithms/data-structures/src/ExamSchedule.java
@@ -0,0 +1,47 @@
+public class ExamSchedule {
+    private ExamNode head;
+    private ExamNode current;
+
+    public ExamSchedule() {
+        this.head = null;
+        this.current = null;
+    }
+
+
+    public void addExam(String examDetails) {
+        // TODO 2: Implement logic to add an exam to the end of the linked list
+    }
+
+
+    public void viewNextExam() {
+        if (current == null) {
+            System.out.println("No exams available.");
+        } else {
+            // TODO 3: Implement the logic to move to the next exam in the list and displays it
+        }
+    }
+
+
+    public void viewPreviousExam() {
+        if (current == null) {
+            System.out.println("No exams available.");
+        } else {
+            // TODO 4: Implement logic to move to the previous exam in the list and display it
+        }
+    }
+
+    public void viewAllExamSchedule() {
+        ExamNode temp = head;
+        if (temp == null) {
+            System.out.println("No exams scheduled.");
+        } else {
+            System.out.println("Exam Schedule:");
+            while (temp != null) {
+                System.out.println(temp.examDetails);
+                temp = temp.next;
+            }
+        }
+    }
+}
+
+
