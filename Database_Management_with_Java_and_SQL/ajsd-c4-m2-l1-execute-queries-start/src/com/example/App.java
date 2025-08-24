bcd24a79a87ee13467beb68613148701030e606f
Moritz Kohr <morris.ko98@googlemail.com>
2025-08-24 14:31:57 +0200

    feat: imported lab code to local workspace

---
+++ Database_Management_with_Java_and_SQL/ajsd-c4-m2-l1-execute-queries-start/src/com/example/App.java
@@ -0,0 +1,29 @@
+package com.example;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+
+        /* TODO 18: Declare a variable newUser of type User and initialize it by calling the constructor and passing the values as arguments betty.george, betty.george@email.com, Betty, George, +11536549870, 15000. */
+
+
+
+        /* TODO 19: Declare a variable userDAO of type UserDAO, initialize it by calling the constructor. */
+
+        /* TODO 20: Call the addUser method using the userDAO and passing the newUser as argument. Store the result in rowsUpdated variable of int type. */
+
+        /* TODO 21: Surround the addUser method call with try-catch block and handle the ClassNotFoundException and SQLException. */
+
+        /* TODO 22: Inside the try block, Use if-else statement to check rowsUpdated variable for value equal to 1.  */
+
+        /* TODO 23: Inside if block, display the message “New user added successfully”. */
+
+        /* TODO 24: Inside else block, display the message “Cannot add user” */
+
+
+
+
+
+    }
+}
