2266aebc4cf881acea8ecbbabbe1e8681458a78d
Moritz Kohr <morris.ko98@googlemail.com>
2025-09-09 15:24:56 +0200

    feat: Updated lab code

--- Full_Stack_Web_Development/ecommerce/src/main/java/com/quickcart/ecommerce/QuickCartApplication.java
+++ Full_Stack_Web_Development/ecommerce/src/main/java/com/quickcart/ecommerce/QuickCartApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuickCartApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuickCartApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(QuickCartApplication.class, args);
+    }
 }
