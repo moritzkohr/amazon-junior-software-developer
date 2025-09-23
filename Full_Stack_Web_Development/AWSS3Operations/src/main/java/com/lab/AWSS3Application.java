fd8b1d186c061b97ecd4586a3ffd7ccd57679649
Moritz Kohr <morris.ko98@googlemail.com>
2025-09-23 14:47:32 +0200

    feat: imported lab code to local workspace

---
+++ Full_Stack_Web_Development/AWSS3Operations/src/main/java/com/lab/AWSS3Application.java
@@ -0,0 +1,13 @@
+package com.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AWSS3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AWSS3Application.class, args);
+    }
+
+}
